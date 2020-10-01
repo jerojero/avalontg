0aa7f4840b980c52d13aa315e1a55114b2e5a453
Michael Bartlett <16125871+mbartlet@users.noreply.github.com>
2020-10-01 11:40:59 -0600

    Forgot $ on CHAT_ID variable

--- telegram_bot_send_message.sh
+++ telegram_bot_send_message.sh
@@ -11,4 +11,4 @@ for v in ${REQUIRED_VARS[@]}; do
 	set $v=${!v-$(1>&2 echo "ERROR: $v is unset, please set this environment variable" ; exit 1)}
 done
 
-curl -s -X GET "https://api.telegram.org/$BOT_ID:$API_KEY/sendMessage" -d parse_mode=html -d chat_id=CHAT_ID -d text="$*" > /dev/null
+curl -s -X GET "https://api.telegram.org/$BOT_ID:$API_KEY/sendMessage" -d parse_mode=html -d chat_id=$CHAT_ID -d text="$*" > /dev/null
